e00d72ba1cf61cfef8fe58d21981edb5764cff0e
pefish <darrondarron@qq.com>
2024-12-18 18:30:21 +0800

    v0.8.5

--- pkg/task/watch_container.go
+++ pkg/task/watch_container.go
@@ -46,11 +46,15 @@ func (t *WatchContainer) Run(ctx context.Context) error {
 	for _, deadProject := range go_format_slice.DeepCopy(global.GlobalData.DeadProjects) {
 		shouldCheck := false
 		for _, project := range projects {
-			containerName := fmt.Sprintf("%s-prod", project.Name)
-			if strings.EqualFold(containerName, deadProject) && project.Status == 1 {
-				shouldCheck = true
-				break
+			ports := strings.Split(project.Port, ",")
+			for i := range ports {
+				containerName := fmt.Sprintf("%s-prod%d", project.Name, i)
+				if strings.EqualFold(containerName, deadProject) && project.Status == 1 {
+					shouldCheck = true
+					break
+				}
 			}
+
 		}
 		if shouldCheck {
 			continue
@@ -68,77 +72,80 @@ func (t *WatchContainer) Run(ctx context.Context) error {
 	}
 
 	for _, project := range projects {
-		containerName := fmt.Sprintf("%s-prod", project.Name)
-		if project.Status == 0 {
-			continue
-		}
-		isAlive := false
-		for _, aliveProject := range aliveProjects {
-			if strings.EqualFold(containerName, aliveProject) {
-				isAlive = true
-				break
+		ports := strings.Split(project.Port, ",")
+		for i := range ports {
+			containerName := fmt.Sprintf("%s-prod%d", project.Name, i)
+			if project.Status == 0 {
+				continue
 			}
-		}
-		if isAlive {
-			if slices.Contains(global.GlobalData.DeadProjects, containerName) {
-				t.logger.InfoF("<%s> 复活，从 deadProjects 中移除", containerName)
-				global.GlobalData.DeadProjects = slices.DeleteFunc(global.GlobalData.DeadProjects, func(containerName_ string) bool {
-					return containerName_ == containerName
-				})
-				util.AlertNoError(t.logger, fmt.Sprintf(`
-项目 <%s> 已复活
-`, containerName))
-			}
-			continue
-		}
-
-		if !slices.Contains(global.GlobalData.DeadProjects, containerName) {
-			t.logger.InfoF("<%s> 意外终止，下次检查如果还处于终止状态，则会报警", containerName)
-			global.GlobalData.DeadProjects = append(global.GlobalData.DeadProjects, containerName)
-
-			// 记录错误信息
-			errorMsg, err := util.FetchErrorMsgFromContainer(containerName)
-			if err != nil {
-				t.logger.InfoF("docker logs 命令执行出错：%+v", err)
-			} else {
-				_, err = global.MysqlInstance.Update(
-					&t_mysql.UpdateParams{
-						TableName: "project",
-						Update: map[string]interface{}{
-							"last_error": errorMsg,
-						},
-						Where: map[string]interface{}{
-							"id": project.Id,
-						},
-					},
-				)
-				if err != nil {
-					t.logger.Info(err)
+			isAlive := false
+			for _, aliveProject := range aliveProjects {
+				if strings.EqualFold(containerName, aliveProject) {
+					isAlive = true
+					break
 				}
 			}
-
-			if project.IsAutoRestart == 1 {
-				err = util.StartContainer(containerName)
-				if err != nil {
-					return err
+			if isAlive {
+				if slices.Contains(global.GlobalData.DeadProjects, containerName) {
+					t.logger.InfoF("<%s> 复活，从 deadProjects 中移除", containerName)
+					global.GlobalData.DeadProjects = slices.DeleteFunc(global.GlobalData.DeadProjects, func(containerName_ string) bool {
+						return containerName_ == containerName
+					})
+					util.AlertNoError(t.logger, fmt.Sprintf(`
+	项目 <%s> 已复活
+	`, containerName))
 				}
-				util.AlertNoError(t.logger, fmt.Sprintf(`
-项目 <%s> 已重启，请关注错误信息
-`, containerName))
+				continue
 			}
-			continue
-		}
 
-		if time.Since(global.GlobalData.LastNotifyTime[containerName]) < 10*time.Minute {
-			t.logger.InfoF("<%s> 短时间内报警过，略过报警", containerName)
-			continue
-		}
+			if !slices.Contains(global.GlobalData.DeadProjects, containerName) {
+				t.logger.InfoF("<%s> 意外终止，下次检查如果还处于终止状态，则会报警", containerName)
+				global.GlobalData.DeadProjects = append(global.GlobalData.DeadProjects, containerName)
 
-		// 警报
-		util.AlertNoError(t.logger, fmt.Sprintf(`
-项目 <%s> 意外终止，请检查
-`, containerName))
-		global.GlobalData.LastNotifyTime[containerName] = time.Now()
+				// 记录错误信息
+				errorMsg, err := util.FetchErrorMsgFromContainer(containerName)
+				if err != nil {
+					t.logger.InfoF("docker logs 命令执行出错：%+v", err)
+				} else {
+					_, err = global.MysqlInstance.Update(
+						&t_mysql.UpdateParams{
+							TableName: "project",
+							Update: map[string]interface{}{
+								"last_error": errorMsg,
+							},
+							Where: map[string]interface{}{
+								"id": project.Id,
+							},
+						},
+					)
+					if err != nil {
+						t.logger.Info(err)
+					}
+				}
+
+				if project.IsAutoRestart == 1 {
+					err = util.StartContainer(containerName)
+					if err != nil {
+						return err
+					}
+					util.AlertNoError(t.logger, fmt.Sprintf(`
+	项目 <%s> 已重启，请关注错误信息
+	`, containerName))
+				}
+				continue
+			}
+
+			if time.Since(global.GlobalData.LastNotifyTime[containerName]) < 10*time.Minute {
+				t.logger.InfoF("<%s> 短时间内报警过，略过报警", containerName)
+				continue
+			}
+
+			// 警报
+			util.AlertNoError(t.logger, fmt.Sprintf(`
+	项目 <%s> 意外终止，请检查
+	`, containerName))
+			global.GlobalData.LastNotifyTime[containerName] = time.Now()
+		}
 	}
 	return nil
 }
